b1e56e365ef28a39fea5d552f21807ecccc879a5
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-04 02:31:42 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -5,9 +5,9 @@ class Project
     @title = title
     @backers = []
   end
-
+  
   def add_backer(backer)
     @backers << backer
- #   backer.back_project(self) unless backer.backed_projects.include?(self)
+    backer.backed_projects << self
   end
 end
