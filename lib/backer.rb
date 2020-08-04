78ee5c6736f9378f41fa4e65d9d4bf264e2abdd8
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-04 02:12:51 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,2 +1,14 @@
 class Backer
   
+attr_accessor :cats, :dogs
+  attr_reader :name, :species
+
+  @@owners = []
+
+# instance methods
+
+  def initialize(backed_projects)
+    @name = name
+    @species = "human"
+    @@owners << self
+    @cats = []
