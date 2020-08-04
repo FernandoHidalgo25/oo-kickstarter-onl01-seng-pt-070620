62d64f76a1f2f6ba2667de7649418a2ae1fa9f73
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-04 02:22:23 +0000

    Automatically backed up by Learn

---
+++ lib/project.rb
@@ -0,0 +1,16 @@
+class Project
+  
+#attr_accessor :cats, :dogs
+  attr_reader :backed_projects, :name, :title
+
+  #@@owners = []
+
+# instance methods
+
+  def initialize(name)
+    @name = name
+    @backed_projects = []
+    @title = title 
+  end 
+end 
+
