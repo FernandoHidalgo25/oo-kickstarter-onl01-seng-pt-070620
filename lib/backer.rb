bfb58cea3e033418567a8a22a4167b73a1b971fe
Fernando Hidalgo <fernandojesus.hidalgominaya9@gmail.com>
2020-08-04 02:31:07 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,22 +1,15 @@
+
 class Backer
-  
-#attr_accessor :cats, :dogs
-  attr_reader :backed_projects, :name, :title
 
-  #@@owners = []
-
-# instance methods
+  attr_accessor :name, :backed_projects
 
   def initialize(name)
     @name = name
     @backed_projects = []
-    @title = title 
-  end 
-  
-    def add_backer(backer)
-    @backers << backer
-  #  backer.back_project(self) unless backer.backed_projects.include?(self)
   end
-    
-end 
 
+  def back_project(project)
+    @backed_projects << project
+    project.backers << self
+  end
+end
